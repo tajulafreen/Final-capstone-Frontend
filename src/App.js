f6c2712e680de8ab15c4cd01726493335edfb427
kit0-0 <kt1767@gmail.com>
2023-11-07 13:55:53 +0530

    Fix linters

--- src/App.js
+++ src/App.js
@@ -1,5 +1,3 @@
-// App.js
-
 import React from 'react';
 import {
   BrowserRouter as Router, Route, Routes,
--- src/components/doctor/DoctorList.js
+++ src/components/doctor/DoctorList.js
@@ -1,5 +1,3 @@
-// DoctorList.js
-
 import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { Link } from 'react-router-dom';
