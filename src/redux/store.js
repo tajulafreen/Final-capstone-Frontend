a1cd5089ca65748d29640315121edc36511adc39
kit0-0 <kt1767@gmail.com>
2023-11-07 13:44:38 +0530

    Add doctorReducer

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,8 +1,9 @@
 import { configureStore } from '@reduxjs/toolkit';
+import doctorReducer from './doctor/doctorSlice';
 
 const store = configureStore({
   reducer: {
-    //    Reducer,
+    doctor: doctorReducer,
   },
 });
 
