b90c15b1cbca480e871e3f5a6946e27fc200ebc1
Diana Beki <dianabeki54@gmail.com>
2023-11-08 01:50:52 +0300

    Add import statements and create doctorlist functional component

--- src/components/doctor/DoctorList.jsx
+++ src/components/doctor/DoctorList.jsx
@@ -1,4 +1,7 @@
-import React from 'react'
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { Link } from 'react-router-dom';
+import { fetchDoctors } from '../../redux/doctor/doctorSlice';
 
 const DoctorList = () => {
   return (
