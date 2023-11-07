4732fefabc9c8f429c4ee474358d635dee098d6e
kit0-0 <kt1767@gmail.com>
2023-11-07 13:42:17 +0530

    Update sidenav

--- src/components/navbar/SideNav.js
+++ src/components/navbar/SideNav.js
@@ -6,22 +6,15 @@ import {
 import { TiSocialGooglePlus } from 'react-icons/ti';
 
 const adminLinks = [
-  { id: 1, path: '/', text: 'MODELS' },
-  { id: 2, path: '/reserve', text: 'RESERVE' },
-  { id: 3, path: '/reservation', text: 'MY RESERVATION' },
-  { id: 4, path: '/add', text: 'ADD' },
-  { id: 5, path: '/delete', text: 'DELETE' },
-];
-
-const links = [
-  { id: 1, path: '/', text: 'MODELS' },
-  { id: 2, path: '/reserve', text: 'RESERVE' },
-  { id: 3, path: '/reservation', text: 'MY RESERVATION' },
+  { id: 1, path: '/doctors', text: 'Doctors' },
+  { id: 2, path: '/newAppointment', text: 'New appointment' },
+  { id: 3, path: '/my-appointment', text: 'My appointment' },
+  { id: 4, path: '/add-doctor', text: 'Add Doctor' },
 ];
 
 const SocialMedia = () => (
-  <div className="absolute bottom-0 start-0">
-    <div className="flex gap-3 p-5">
+  <div className="absolute bottom-0 start-0 w-full">
+    <div className="flex gap-3 p-3">
       <FaTwitter className="text-gray-700" />
       <FaFacebookF className="text-gray-700" />
       <TiSocialGooglePlus className="text-gray-700" />
@@ -29,44 +22,29 @@ const SocialMedia = () => (
       <FaPinterestP className="text-gray-700" />
     </div>
     <div>
-      <p className="text-sm">&copy; 2023</p>
+      <p className="text-sm ps-5">&copy; 2023</p>
     </div>
   </div>
 );
 
-const SideNav = () => {
-  const data = JSON.parse(localStorage.getItem('user'));
-  const { user } = data || {};
-  return (
-    <div className="bg-light">
-      <div className="w-200 h-screen">
-        <ul className="flex flex-col justify-center items-end mt-14">
-          {user && user.role === 'admin' ? (adminLinks.map((link) => (
-            <li key={link.id}>
-              <NavLink
-                to={link.path}
-                className="btn-light btn w-full"
-                activeClassName="bg-green-500 text-white"
-              >
-                {link.text}
-              </NavLink>
-            </li>
-          ))) : links.map((link) => (
-            <li key={link.id}>
-              <NavLink
-                to={link.path}
-                className="btn-light btn w-full"
-                activeClassName="bg-green-500 text-white"
-              >
-                {link.text}
-              </NavLink>
-            </li>
-          ))}
-        </ul>
-        <SocialMedia />
-      </div>
+const SideNav = () => (
+  <div className="border-r-2 h-screen">
+    <div className="w-200">
+      <ul className="flex flex-col justify-center items-end mt-14 space-y-2">
+        {adminLinks.map((link) => (
+          <li key={link.id}>
+            <NavLink
+              to={link.path}
+              className={({ isActive }) => `text-gray-700 py-2 px-4 no-underline${isActive ? ' my-active-class bg-green-500 text-white' : ''}`}
+            >
+              {link.text}
+            </NavLink>
+          </li>
+        ))}
+      </ul>
+      <SocialMedia />
     </div>
-  );
-};
+  </div>
+);
 
 export default SideNav;
