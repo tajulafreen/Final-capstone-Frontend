654ae91def021a37148e0801c64e8643fe9073ea
Diana Beki <dianabeki54@gmail.com>
2023-11-08 01:35:14 +0300

    Sends post request to create new doctor on the server

--- src/redux/doctor/doctorSlice.js
+++ src/redux/doctor/doctorSlice.js
@@ -47,32 +47,7 @@ const doctorSlice = createSlice({
         state.status = 'succeeded';
         state.doctors.push(action.payload);
       })
-      .addCase(fetchDoctors.fulfilled, (state, action) => {
-        state.status = 'succeeded';
-        state.doctors = action.payload;
-      })
-      .addCase(fetchDoctorById.fulfilled, (state, action) => {
-        state.status = 'succeeded';
-        state.doctors = [action.payload];
-      })
-      .addCase(deleteDoctor.fulfilled, (state, action) => {
-        state.status = 'succeeded';
-        state.doctors = state.doctors.filter((doctor) => doctor.id !== action.payload);
-      })
-      .addMatcher(
-        (action) => action.type.endsWith('/pending'),
-        (state) => {
-          state.status = 'loading';
-        },
-      )
-      .addMatcher(
-        (action) => action.type.endsWith('/rejected'),
-        (state, action) => {
-          state.status = 'failed';
-          state.error = action.error.message;
-        },
-      );
-  },
+     
 });
 
 export const selectDoctors = (state) => state.doctor.doctors;
