20a4696b0eb46620f73a56924b583087776beece
afreen shaik <affushaik9493@gmail.com>
2023-11-09 22:01:14 +0530

    add signup function

--- src/redux/user/userSlice.js
+++ src/redux/user/userSlice.js
@@ -1,2 +1,11 @@
 import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
 import axios from 'axios';
+
+export const signUpUser = createAsyncThunk('user/signUp', async (userData) => {
+    try {
+      const response = await axios.post('http://localhost:3000/sign_up', userData);
+      return response.data;
+    } catch (error) {
+      throw error.response.data;
+    }
+  });
