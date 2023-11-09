10d0b37d452d5486039d4579df22237e3515bf19
afreen shaik <affushaik9493@gmail.com>
2023-11-09 22:06:44 +0530

    solve linters

--- src/redux/user/userSlice.js
+++ src/redux/user/userSlice.js
@@ -2,49 +2,48 @@ import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
 import axios from 'axios';
 
 export const signUpUser = createAsyncThunk('user/signUp', async (userData) => {
-    try {
-      const response = await axios.post('http://localhost:3000/sign_up', userData);
-      return response.data;
-    } catch (error) {
-      throw error.response.data;
-    }
-  });
-  export const signInUser = createAsyncThunk('user/signIn', async (userData) => {
-    try {
-      const response = await axios.post('http://localhost:3000/login', userData);
-      return response.data;
-    } catch (error) {
-      throw error.response.data;
-    }
-  });
+  try {
+    const response = await axios.post('http://localhost:3000/sign_up', userData);
+    return response.data;
+  } catch (error) {
+    throw error.response.data;
+  }
+});
+export const signInUser = createAsyncThunk('user/signIn', async (userData) => {
+  try {
+    const response = await axios.post('http://localhost:3000/login', userData);
+    return response.data;
+  } catch (error) {
+    throw error.response.data;
+  }
+});
 
-  const userSlice = createSlice({
-    name: 'user',
-    initialState: { user: null, status: 'idle', error: null },
-    reducers: {},
-    extraReducers: (builder) => {
-      builder
-        .addCase(signUpUser.fulfilled, (state, action) => {
-          state.status = 'succeeded';
-          state.user = action.payload;
-        })
-        .addCase(signInUser.fulfilled, (state, action) => {
-            state.status = 'succeeded';
-            state.user = action.payload;
-          })
-          .addMatcher(
-            (action) => action.type.endsWith('/pending'),
-            (state) => {
-              state.status = 'loading';
-            },
-          )
-          .addMatcher(
-            (action) => action.type.endsWith('/rejected'),
-            (state, action) => {
-              state.status = 'failed';
-              state.error = action.error.message;
-            },
-          );
-      },
-    });
-    
+const userSlice = createSlice({
+  name: 'user',
+  initialState: { user: null, status: 'idle', error: null },
+  reducers: {},
+  extraReducers: (builder) => {
+    builder
+      .addCase(signUpUser.fulfilled, (state, action) => {
+        state.status = 'succeeded';
+        state.user = action.payload;
+      })
+      .addCase(signInUser.fulfilled, (state, action) => {
+        state.status = 'succeeded';
+        state.user = action.payload;
+      })
+      .addMatcher(
+        (action) => action.type.endsWith('/pending'),
+        (state) => {
+          state.status = 'loading';
+        },
+      )
+      .addMatcher(
+        (action) => action.type.endsWith('/rejected'),
+        (state, action) => {
+          state.status = 'failed';
+          state.error = action.error.message;
+        },
+      );
+  },
+});
