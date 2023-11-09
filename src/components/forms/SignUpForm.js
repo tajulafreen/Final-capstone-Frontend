bbbd1f1878dda50d9c668169f2fe3fa3f324f9d7
afreen shaik <affushaik9493@gmail.com>
2023-11-09 22:16:35 +0530

    add sign up form

--- src/components/forms/SignUpForm.js
+++ src/components/forms/SignUpForm.js
@@ -2,3 +2,11 @@ import React, { useState } from 'react';
 import { useDispatch } from 'react-redux';
 import { useNavigate } from 'react-router-dom';
 import { signUpUser } from '../../redux/user/userSlice';
+
+const SignUpForm = () => {
+    const dispatch = useDispatch();
+    const navigate = useNavigate();
+    const [name, setName] = useState('');
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+  
