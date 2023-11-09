fa341574e400b0707d1be86e6a9d7b25f5b5c972
afreen shaik <affushaik9493@gmail.com>
2023-11-09 22:29:34 +0530

    solve linter issue

--- src/components/forms/SignUpForm.js
+++ src/components/forms/SignUpForm.js
@@ -4,29 +4,29 @@ import { useNavigate } from 'react-router-dom';
 import { signUpUser } from '../../redux/user/userSlice';
 
 const SignUpForm = () => {
-    const dispatch = useDispatch();
-    const navigate = useNavigate();
-    const [name, setName] = useState('');
-    const [email, setEmail] = useState('');
-    const [password, setPassword] = useState('');
-  
-    const handleSignUp = async (e) => {
-        e.preventDefault();
-    
-        try {
-          await dispatch(signUpUser({ name, email, password }));
-          navigate.push('/doctors');
-        } catch (error) {
-          console.error('Sign Up Error:', error);
-        }
-      };
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
 
-      return (
-        <div className="flex items-center justify-center h-screen">
-          <form className="bg-white p-8 shadow-md rounded-md w-96" onSubmit={handleSignUp}>
-            <h2 className="text-2xl font-semibold mb-6">Sign Up</h2>
-            <div className="mb-4">
-            <input
+  const handleSignUp = async (e) => {
+    e.preventDefault();
+
+    try {
+      await dispatch(signUpUser({ name, email, password }));
+      navigate.push('/doctors');
+    } catch (error) {
+      console.error('Sign Up Error:', error);
+    }
+  };
+
+  return (
+    <div className="flex items-center justify-center h-screen">
+      <form className="bg-white p-8 shadow-md rounded-md w-96" onSubmit={handleSignUp}>
+        <h2 className="text-2xl font-semibold mb-6">Sign Up</h2>
+        <div className="mb-4">
+          <input
             type="text"
             id="name"
             name="name"
@@ -35,7 +35,7 @@ const SignUpForm = () => {
             className="mt-1 p-2 w-full border rounded-md"
             placeholder="Enter your name"
           />
-            </div>
+        </div>
         <div className="mb-4">
           <input
             type="email"
@@ -46,7 +46,7 @@ const SignUpForm = () => {
             className="mt-1 p-2 w-full border rounded-md"
             placeholder="Enter your email"
           />
-          </div>
+        </div>
         <div className="mb-4">
           <input
             type="password"
@@ -57,7 +57,7 @@ const SignUpForm = () => {
             className="mt-1 p-2 w-full border rounded-md"
             placeholder="Enter your password"
           />
-          </div>
+        </div>
         <button
           type="submit"
           className="bg-blue-500 text-white px-4 py-2 rounded-md hover:bg-blue-600"
