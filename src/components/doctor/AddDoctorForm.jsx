3d947692f6694058dd5709d749180f9c5998e429
Diana Beki <dianabeki54@gmail.com>
2023-11-09 16:43:06 +0300

    create form structure with input field for doctor's name

--- src/components/doctor/AddDoctorForm.jsx
+++ src/components/doctor/AddDoctorForm.jsx
@@ -31,6 +31,19 @@ const AddDoctorForm = () => {
     <div className="max-w-md mx-auto mt-8 p-6 bg-white shadow-md rounded-md">
        <h2 className="text-2xl font-bold mb-4 text-center">Add New Doctor</h2>
       {status === 'failed' && <div className="text-red-500 mb-4 text-center">{error}</div>}
+      <form onSubmit={handleFormSubmit}>
+      <div className="mb-4">
+          <input
+            type="text"
+            name="name"
+            placeholder="Name"
+            value={formData.name}
+            onChange={handleInputChange}
+            required
+            className="mt-1 p-2 border rounded-md w-full focus:outline-none focus:ring focus:border-blue-300"
+          />
+        </div>
+      </form>
     </div>
   )
 }
