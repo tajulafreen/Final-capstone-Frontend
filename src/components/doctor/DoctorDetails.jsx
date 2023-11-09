94831cdd0127bea316d798fc11d04cafff8e5f54
kit0-0 <kt1767@gmail.com>
2023-11-09 20:48:07 +0530

    Initialize DoctorDetails component and import dependencies

--- src/components/doctor/DoctorDetails.jsx
+++ src/components/doctor/DoctorDetails.jsx
@@ -1,7 +1,9 @@
-import React from 'react';
-
-const DoctorDetails = () => (
-  <div />
-);
-
-export default DoctorDetails;
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { useParams, useNavigate, Link } from 'react-router-dom';
+import {
+  fetchDoctorById,
+  selectDoctors,
+  selectStatus,
+  selectError,
+} from '../../redux/doctor/doctorSlice';
