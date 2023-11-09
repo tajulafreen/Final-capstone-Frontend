60a67dc5f2f2584bc52fbb6a32151e9fe89742c4
Diana Beki <dianabeki54@gmail.com>
2023-11-09 17:07:00 +0300

    Fix linters error

--- src/components/doctor/AddDoctorForm.jsx
+++ src/components/doctor/AddDoctorForm.jsx
@@ -87,7 +87,7 @@ const AddDoctorForm = () => {
         <button
           type="submit"
           disabled={status === 'loading'}
-          className="bg-green-500 hover:bg-blue-500 text-white hover:text-black px-4 py-2 rounded-md focus:outline-none focus:ring focus:border-blue-300 w-full"
+          className="bg-green-500 hover:bg-blue-500 text-white px-4 py-2 rounded-md focus:outline-none focus:ring focus:border-blue-300 w-full"
         >
           {status === 'loading' ? 'Adding...' : 'Add Doctor'}
         </button>
