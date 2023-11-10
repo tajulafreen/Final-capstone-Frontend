ac3c7a784eec036a07fa8b8b7842f0d84bf64b25
afreen shaik <affushaik9493@gmail.com>
2023-11-11 02:37:26 +0530

    update deletepage

--- src/components/doctor/DeleteDoctor.jsx
+++ src/components/doctor/DeleteDoctor.jsx
@@ -1,7 +1,9 @@
 import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { useParams, useHistory } from 'react-router-dom';
-import { fetchDoctorById, deleteDoctor, selectDoctors, selectStatus, selectError } from '../../redux/doctor/doctorSlice';
+import {
+  fetchDoctorById, deleteDoctor, selectDoctors, selectStatus, selectError,
+} from '../../redux/doctor/doctorSlice';
 
 const DeleteDoctorPage = () => {
   const dispatch = useDispatch();
@@ -18,7 +20,7 @@ const DeleteDoctorPage = () => {
   const handleDelete = async () => {
     try {
       await dispatch(deleteDoctor(doctorId));
-      history.push('/doctors'); 
+      history.push('/doctors');
     } catch (err) {
       console.error('Error deleting doctor:', err);
     }
@@ -27,12 +29,22 @@ const DeleteDoctorPage = () => {
   return (
     <div className="container mx-auto my-4 p-4 bg-white shadow-lg rounded-lg">
       {status === 'loading' && <p>Loading...</p>}
-      {status === 'failed' && <p className="text-red-500">Error: {error}</p>}
+      {status === 'failed' && (
+      <p className="text-red-500">
+        Error:
+        {error}
+      </p>
+      )}
       {status === 'succeeded' && (
         <div>
           <h2 className="text-2xl font-bold mb-4">Delete Doctor</h2>
-          <p>Are you sure you want to delete {doctor.name}?</p>
+          <p>
+            Are you sure you want to delete
+            {doctor.name}
+            ?
+          </p>
           <button
+            type="button"
             className="bg-red-500 text-white px-4 py-2 rounded mt-4 hover:bg-red-600"
             onClick={handleDelete}
           >
