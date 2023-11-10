ac843c06feac29e52335c2ad853fe0d813f18d1f
afreen shaik <affushaik9493@gmail.com>
2023-11-11 01:40:53 +0530

    add delete doctor page

---
+++ src/components/doctor/DeleteDoctor.jsx
@@ -0,0 +1,14 @@
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { useParams, useHistory } from 'react-router-dom';
+import { fetchDoctorById, deleteDoctor, selectDoctors, selectStatus, selectError } from '../../redux/doctor/doctorSlice';
+
+const DeleteDoctorPage = () => {
+  const dispatch = useDispatch();
+  const history = useHistory();
+  const { doctorId } = useParams();
+  const doctor = useSelector(selectDoctors)[0];
+  const status = useSelector(selectStatus);
+  const error = useSelector(selectError);
+
+  
