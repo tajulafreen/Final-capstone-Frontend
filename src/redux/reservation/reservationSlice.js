d303f233cacae5dd8df2a28154d59f728b487d60
Foga Kater Amos <katfogy@gmail.com>
2023-11-14 14:21:41 +0530

    Update reservationSlice case

--- src/redux/reservation/reservationSlice.js
+++ src/redux/reservation/reservationSlice.js
@@ -18,6 +18,10 @@ const reservationsSlice = createSlice({
         state.isLoading = true;
         state.error = false;
       })
+      .addCase(fetchReservations.fulfilled, (state, action) => {
+        state.isLoading = false;
+        state.reservations = action.payload;
+      })
      
   },
 });
