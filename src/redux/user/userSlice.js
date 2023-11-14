0dc6650538d9fe960ed7d898925e06f8d03b9b99
Foga Kater Amos <katfogy@gmail.com>
2023-11-14 17:35:43 +0530

    Update state

--- src/redux/user/userSlice.js
+++ src/redux/user/userSlice.js
@@ -55,8 +55,6 @@ const userSlice = createSlice({
       );
   },
 });
-export const selectUser = (state) => state.user.user;
-export const selectStatus = (state) => state.user.status;
-export const selectError = (state) => state.user.error;
+
 
 export default userSlice.reducer;
