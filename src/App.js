38794e144d5f369bbc3ddf8574cb0c8e1e7b055e
Diana Beki <dianabeki54@gmail.com>
2023-11-15 16:39:58 +0300

    Add margin left to containernav

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ const App = () => (
   <Router>
     <div className="flex">
       <SideNav />
-      <div className="flex-1">
+      <div className="flex-1 ml-40 p-8">
         <Routes>
           <Route path="/" element={<SignUpForm />} />
           <Route path="/login" element={<SignInForm />} />
