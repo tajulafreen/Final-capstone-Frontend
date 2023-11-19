66f90300621b92bf9da1d3595f2eace0e637bd63
afreen shaik <affushaik9493@gmail.com>
2023-11-20 02:21:45 +0530

    create snapshots and solve error related route

--- src/test/DoctorDetails.test.js
+++ src/test/DoctorDetails.test.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import { render as rtlRender } from '@testing-library/react';
 import { Provider } from 'react-redux';
+import { BrowserRouter as Router } from 'react-router-dom';
 import DoctorDetails from '../components/doctor/DoctorDetails';
 import store from '../redux/store';
 
 const render = (component) => rtlRender(
   <Provider store={store}>
-    {component}
+    <Router>
+      {component}
+    </Router>
   </Provider>,
 );
 
--- src/test/newReservation.test.js
+++ src/test/newReservation.test.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import { render as rtlRender } from '@testing-library/react';
 import { Provider } from 'react-redux';
+import { BrowserRouter as Router } from 'react-router-dom';
 import NewReservation from '../components/pages/newReservation';
 import store from '../redux/store';
 
 const render = (component) => rtlRender(
   <Provider store={store}>
-    {component}
+    <Router>
+      {component}
+    </Router>
   </Provider>,
 );
 
