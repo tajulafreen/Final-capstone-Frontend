d45c5473695e7b99ae0497233461ae61cacf2942
afreen shaik <affushaik9493@gmail.com>
2023-11-20 20:14:51 +0530

    add navbar link

--- src/components/doctor/DeleteDoctor.jsx
+++ src/components/doctor/DeleteDoctor.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { fetchDoctors, deleteDoctor } from '../../redux/doctor/doctorSlice';
+import SideNav from '../navbar/SideNav';
 
 const DeleteDoctor = () => {
   const { doctors, status, error } = useSelector((store) => store.doctor);
@@ -15,40 +16,43 @@ const DeleteDoctor = () => {
   };
 
   return (
-    <div className="text-center">
-      <h1 className="mb-2 font-bold text-[14px] md:text-[25px] text-[#1a1a1a] md:mb-5">Delete Doctor</h1>
+    <>
+      <SideNav />
+      <div className="text-center">
+        <h1 className="mb-2 font-bold text-[14px] md:text-[25px] text-[#1a1a1a] md:mb-5">Delete Doctor</h1>
 
-      {status === 'loading' && <p>Loading...</p>}
-      {status === 'failed' && (
-      <p>
-        Error:
-        {error}
-      </p>
-      )}
+        {status === 'loading' && <p>Loading...</p>}
+        {status === 'failed' && (
+        <p>
+          Error:
+          {error}
+        </p>
+        )}
 
-      {doctors.length === 0 && status === 'succeeded' ? (
-        <h1 className="text-xl mt-20 text-slate-500">No doctors Added!!</h1>
-      ) : (
-        doctors.map((doctor) => (
-          <div key={doctor.id} className="flex flex-col justify-center items-center py-[.8rem] md:gap-5 md:grid md:grid-cols-2 md:px-[8rem] md:py-[1.2rem]">
-            <img src={doctor.image} alt={doctor.name} className="object-cover w-83" />
-            <div>
+        {doctors.length === 0 && status === 'succeeded' ? (
+          <h1 className="text-xl mt-20 text-slate-500">No doctors Added!!</h1>
+        ) : (
+          doctors.map((doctor) => (
+            <div key={doctor.id} className="flex flex-col justify-center items-center py-[.8rem] md:gap-5 md:grid md:grid-cols-2 md:px-[8rem] md:py-[1.2rem]">
+              <img src={doctor.image} alt={doctor.name} className="object-cover w-83" />
               <div>
-                <p className="font-bold text-[20px]">{doctor.name}</p>
-                <p className="text-[10px] text-gray-500 font-semi-bold md:text-[18px] md:leading-[30px]">{doctor.specialization}</p>
+                <div>
+                  <p className="font-bold text-[20px]">{doctor.name}</p>
+                  <p className="text-[10px] text-gray-500 font-semi-bold md:text-[18px] md:leading-[30px]">{doctor.specialization}</p>
+                </div>
+                <button
+                  type="button"
+                  className="bg-lime-500 hover:bg-gray-400 text-white px-1.5 md:px-4 md:py-2 rounded-md transition-transform transform hover:scale-105 duration-500"
+                  onClick={() => { handleDelete(doctor.id); }}
+                >
+                  Delete
+                </button>
               </div>
-              <button
-                type="button"
-                className="bg-lime-500 hover:bg-gray-400 text-white px-1.5 md:px-4 md:py-2 rounded-md transition-transform transform hover:scale-105 duration-500"
-                onClick={() => { handleDelete(doctor.id); }}
-              >
-                Delete
-              </button>
             </div>
-          </div>
-        ))
-      )}
-    </div>
+          ))
+        )}
+      </div>
+    </>
   );
 };
 
--- src/components/doctor/DoctorList.jsx
+++ src/components/doctor/DoctorList.jsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { Link } from 'react-router-dom';
 import { fetchDoctors } from '../../redux/doctor/doctorSlice';
+import SideNav from '../navbar/SideNav';
 
 const DoctorList = () => {
   const dispatch = useDispatch();
@@ -39,77 +40,80 @@ const DoctorList = () => {
   });
 
   return (
-    <div className="flex flex-col justify-center items-center">
-      <h1 className="text-[#1F1717]">Doctors List</h1>
-      <span className="text-gray-400">Choose a doctor</span>
-      {status === 'loading' && <p>Loading...</p>}
-      {status === 'failed' && (
-        <p>
-          Error:
-          {error}
-        </p>
-      )}
-      {(status === 'succeeded' && doctors.length > 0) ? (
-        <>
-          <ul className="gap-[2rem] md:flex md:gap-[5rem] md:mt-10">
-            <div className="flex md:flex-row gap-11">
-              <div className="flex align-items-center">
-                <button
-                  type="button"
-                  onClick={handlePrev}
-                  disabled={startIndex === 0}
-                  className="prev-btn bg-lime-500 text-white p-1.5 md:p-3 disabled:bg-lime-200 border rounded-md"
-                >
-                  &lt;
-                </button>
-              </div>
-              {paginatedDoctors.map((doctor) => (
-                <li
-                  key={doctor.id}
-                  className={`my-[2rem] md:my-0 md:transition-transform md:transform md:hover:scale-110 md:duration-500 ${
-                    window.innerWidth >= 768 ? 'md:w-1/3' : 'w-full'
-                  }`}
-                >
-                  <Link to={`/doctors/${doctor.id}`} className="no-underline">
-                    <div className="flex items-center justify-center">
-                      <img
-                        src={doctor.image}
-                        alt={doctor.name}
-                        className="rounded-full object-cover w-72 h-72"
-                      />
-                    </div>
-                    <div className="gap-0 flex flex-col justify-center items-center md:gap-1 mt-7">
-                      <div className="text-[#1F1717]">
-                        <h2 className="text-[25px]">
-                          {doctor.name}
-                        </h2>
+    <>
+      <SideNav />
+      <div className="flex flex-col justify-center items-center">
+        <h1 className="text-[#1F1717]">Doctors List</h1>
+        <span className="text-gray-400">Choose a doctor</span>
+        {status === 'loading' && <p>Loading...</p>}
+        {status === 'failed' && (
+          <p>
+            Error:
+            {error}
+          </p>
+        )}
+        {(status === 'succeeded' && doctors.length > 0) ? (
+          <>
+            <ul className="gap-[2rem] md:flex md:gap-[5rem] md:mt-10">
+              <div className="flex md:flex-row gap-11">
+                <div className="flex align-items-center">
+                  <button
+                    type="button"
+                    onClick={handlePrev}
+                    disabled={startIndex === 0}
+                    className="prev-btn bg-lime-500 text-white p-1.5 md:p-3 disabled:bg-lime-200 border rounded-md"
+                  >
+                    &lt;
+                  </button>
+                </div>
+                {paginatedDoctors.map((doctor) => (
+                  <li
+                    key={doctor.id}
+                    className={`my-[2rem] md:my-0 md:transition-transform md:transform md:hover:scale-110 md:duration-500 ${
+                      window.innerWidth >= 768 ? 'md:w-1/3' : 'w-full'
+                    }`}
+                  >
+                    <Link to={`/doctors/${doctor.id}`} className="no-underline">
+                      <div className="flex items-center justify-center">
+                        <img
+                          src={doctor.image}
+                          alt={doctor.name}
+                          className="rounded-full object-cover w-72 h-72"
+                        />
                       </div>
-                      <div className="text-[10px] text-gray-700 font-semi-bold md:text-[18px] md:leading-[30px]">
-                        {doctor.specialization}
+                      <div className="gap-0 flex flex-col justify-center items-center md:gap-1 mt-7">
+                        <div className="text-[#1F1717]">
+                          <h2 className="text-[25px]">
+                            {doctor.name}
+                          </h2>
+                        </div>
+                        <div className="text-[10px] text-gray-700 font-semi-bold md:text-[18px] md:leading-[30px]">
+                          {doctor.specialization}
+                        </div>
                       </div>
-                    </div>
-                  </Link>
-                </li>
-              ))}
-              <div className="d-flex align-items-center">
-                <button
-                  type="button"
-                  onClick={handleNext}
-                  disabled={startIndex + getItemsPerPage() >= doctors.length}
-                  className="next-btn bg-lime-500 text-white p-1.5 md:p-3 disabled:bg-lime-200 border rounded-md"
-                >
-                  &gt;
-                </button>
+                    </Link>
+                  </li>
+                ))}
+                <div className="d-flex align-items-center">
+                  <button
+                    type="button"
+                    onClick={handleNext}
+                    disabled={startIndex + getItemsPerPage() >= doctors.length}
+                    className="next-btn bg-lime-500 text-white p-1.5 md:p-3 disabled:bg-lime-200 border rounded-md"
+                  >
+                    &gt;
+                  </button>
+                </div>
               </div>
-            </div>
-          </ul>
-        </>
-      ) : (
-        (status === 'succeeded' && doctors.length === 0) && (
-          <p className="text-xl mt-5 text-slate-500">No Doctor Available</p>
-        )
-      )}
-    </div>
+            </ul>
+          </>
+        ) : (
+          (status === 'succeeded' && doctors.length === 0) && (
+            <p className="text-xl mt-5 text-slate-500">No Doctor Available</p>
+          )
+        )}
+      </div>
+    </>
   );
 };
 
