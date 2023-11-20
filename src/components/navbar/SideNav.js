2d4f04d453c49260a1bbebb81f9969f645ec6384
Diana Beki <dianabeki54@gmail.com>
2023-11-20 12:03:44 +0300

    Implement responsive navigation sidebar

--- src/components/navbar/SideNav.js
+++ src/components/navbar/SideNav.js
@@ -1,9 +1,11 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { NavLink } from 'react-router-dom';
+
 import {
-  FaTwitter, FaFacebookF, FaPinterestP, FaVimeoV,
+  FaTwitter, FaFacebookF, FaPinterestP, FaVimeoV, FaBars, FaTimes,
 } from 'react-icons/fa';
 import { TiSocialGooglePlus } from 'react-icons/ti';
+import './nav.css';
 
 const adminLinks = [
   { id: 1, path: '/doctors', text: 'Doctors' },
@@ -28,22 +30,59 @@ const SocialMedia = () => (
   </div>
 );
 
-const SideNav = () => (
-  <div className=" hidden md:block fixed h-screen bg-white border-r border-gray-300 w-30">
-    <ul className="flex flex-col items-end font-bold text-[#000000] gap-4 uppercase mt-[10rem]">
-      {adminLinks.map((link) => (
-        <li key={link.id}>
-          <NavLink
-            to={link.path}
-            className={({ isActive }) => `text-gray-700 py-2 px-4 no-underline${isActive ? ' my-active-class bg-lime-500 text-white' : ''}`}
-          >
-            {link.text}
-          </NavLink>
-        </li>
-      ))}
-    </ul>
-    <SocialMedia />
-  </div>
-);
+const SideNav = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const handleToggle = () => {
+    setIsOpen(!isOpen);
+  };
+
+  const handleClick = () => {
+    if (window.innerWidth <= 768) {
+      setIsOpen(false);
+    }
+  };
+
+  return (
+    <div className={`menu-bar ${isOpen ? 'open-btn' : 'close-btn'}`}>
+      <div className="hidden md:block fixed h-screen bg-white md:border-r md:border-gray-300">
+        <ul className="md:flex md:flex-col md:items-end font-bold text-[#000000] md:gap-4 uppercase mt-[10rem]">
+          {adminLinks.map((link) => (
+            <li key={link.id}>
+              <NavLink
+                to={link.path}
+                className={({ isActive }) => `text-gray-700 py-2 px-4 no-underline${isActive ? ' my-active-class bg-lime-500 text-white' : ''}`}
+              >
+                {link.text}
+              </NavLink>
+            </li>
+          ))}
+        </ul>
+        <SocialMedia />
+      </div>
+      <button type="button" onClick={handleToggle} className="menu-icon">
+        {isOpen ? <FaTimes className="close" /> : <FaBars className="open" />}
+      </button>
+      {isOpen && (
+        <div className="fabarLinks">
+          <ul className="flex flex-col items-end font-bold text-[#000000] gap-4 uppercase mt-[10rem]">
+            {adminLinks.map((link) => (
+              <li key={link.id} className="links">
+                <NavLink
+                  to={link.path}
+                  onClick={handleClick}
+                  className={({ isActive }) => `text-gray-700 py-2 px-4 no-underline${isActive ? ' my-active-class bg-lime-500 text-white' : ''}`}
+                >
+                  {link.text}
+                </NavLink>
+              </li>
+            ))}
+            <SocialMedia />
+          </ul>
+        </div>
+      )}
+    </div>
+  );
+};
 
 export default SideNav;
