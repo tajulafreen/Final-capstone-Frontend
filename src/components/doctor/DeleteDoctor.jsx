75a79c71e8cae8da8570492b7d160ecaf28a17a2
afreen shaik <affushaik9493@gmail.com>
2023-11-21 23:01:25 +0530

    modify styles

--- src/components/doctor/DeleteDoctor.jsx
+++ src/components/doctor/DeleteDoctor.jsx
@@ -19,7 +19,7 @@ const DeleteDoctor = () => {
     <>
       <SideNav />
       <div className="text-center">
-        <h1 className="mb-2 font-bold text-[14px] md:text-[25px] text-[#1a1a1a] md:mb-5">Delete Doctor</h1>
+        <h2 className="m-7 font-bold text-25px font-semi-bold md:mb-5 text-center md:pl-[9rem]">Delete Doctor</h2>
 
         {status === 'loading' && <p>Loading...</p>}
         {status === 'failed' && (
@@ -33,8 +33,8 @@ const DeleteDoctor = () => {
           <h1 className="text-xl mt-20 text-slate-500">No doctors Added!!</h1>
         ) : (
           doctors.map((doctor) => (
-            <div key={doctor.id} className="flex flex-col justify-center items-center py-[.8rem] gap-[1rem] md:grid md:grid-cols-2 md:px-[18rem] md:py-[1.2rem]">
-              <img src={doctor.image} alt={doctor.name} className="object-cover" />
+            <div key={doctor.id} className="flex flex-col justify-center items-center py-[.8rem] gap-[1rem]  md:flex-row md:gap-[8rem] md:pl-[15rem] md:py-[1.2rem]">
+              <img src={doctor.image} alt={doctor.name} className="rounded-full object-cover w-72 h-72 max-[967px]:w-62" />
               <div>
                 <div>
                   <p className="font-bold text-[20px]">{doctor.name}</p>
@@ -42,7 +42,7 @@ const DeleteDoctor = () => {
                 </div>
                 <button
                   type="button"
-                  className="bg-lime-500 hover:bg-gray-400 text-white px-3 md:px-10 py-2 rounded-md transition-transform transform hover:scale-105 duration-500"
+                  className="bg-lime-500 hover:bg-red-500 text-white px-3 md:px-10 py-2 rounded-md transition-transform transform hover:scale-105 duration-500"
                   onClick={() => { handleDelete(doctor.id); }}
                 >
                   Delete
